62ee4647b12a7d342c0bd2eaa65c93f00921fd21
jacksonhuish88 <jacksonhuish88@gmail.com>
2025-11-19 10:20:58 -0700

    Initial Commit

---
+++ RelateAi/RelateAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelateAiApp.swift
+//  RelateAi
+//
+//  Created by Jackson Huish on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct RelateAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
